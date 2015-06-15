7496194e33a7b01fa6402ac423f1878fa892ce9c
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2015-06-16 07:36:06 +0900

    clean

--- main.c
+++ main.c
@@ -37,47 +37,7 @@ void led_blink(void)
 		}
 	}
 }
-/*
-int main(int argc, char **argv)
-{
-	pthread_t id;
-	int ret;
-	void *virtual_base;
-	int fd;
-	// map the address space for the LED registers into user space so we can interact with them.
-	// we'll actually map in the entire CSR span of the HPS since we want to access various registers within that span
-	if( ( fd = open( "/dev/mem", ( O_RDWR | O_SYNC ) ) ) == -1 ) {
-		printf( "ERROR: could not open \"/dev/mem\"...\n" );
-		return( 1 );
-	}
-	virtual_base = mmap( NULL, HW_REGS_SPAN, ( PROT_READ | PROT_WRITE ), MAP_SHARED, fd, HW_REGS_BASE );
-	if( virtual_base == MAP_FAILED ) {
-		printf( "ERROR: mmap() failed...\n" );
-		close( fd );
-		return(1);
-	}
-	h2p_lw_led_addr=virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + LED_PIO_BASE ) & ( unsigned long)( HW_REGS_MASK ) );
-	h2p_lw_hex_addr=virtual_base + ( ( unsigned long  )( ALT_LWFPGASLVS_OFST + SEG7_IF_BASE ) & ( unsigned long)( HW_REGS_MASK ) );
-	ret=pthread_create(&id,NULL,(void *)led_blink,NULL);
-	if(ret!=0){
-		printf("Creat pthread error!\n");
-		exit(1);
-	}
-	while(1)
-	{
-		SEG7_All_Number();
-	}
-	pthread_join(id,NULL);
-	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
-		printf( "ERROR: munmap() failed...\n" );
-		close( fd );
-		return( 1 );
 
-	}
-	close( fd );
-	return 0;
-}
-*/
 int main(int argc, char **argv)
 {
 	pthread_t id;
