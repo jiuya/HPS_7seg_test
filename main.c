bb116fcbf0240ec6ccb8ce5e8b7798be28e138b7
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2015-06-16 05:16:56 +0900

    fix

--- main.c
+++ main.c
@@ -109,7 +109,7 @@ int main(int argc, char **argv)
 	while(1)
 	{
 		//SEG7_All_Number();
-		alt_write_word(h2p_lw_7seg_addr,0x1234);
+		alt_write_word(h2p_lw_7seg_addr,0x123456);
 	}
 	//pthread_join(id,NULL);
 	if( munmap( virtual_base, HW_REGS_SPAN ) != 0 ) {
